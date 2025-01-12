7131fb1c4064a4aa3d1c3d4a6799ab9d9d2e4c8a
Saiful I <saifulis.4965@gmail.com>
2025-01-12 17:31:29 +0530

    home page

--- ecommerce/core/apps.py
+++ ecommerce/core/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class CorsConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cors'
+    name = 'core'
