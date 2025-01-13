93e86b96fd65f9e7669700f40475af97fcaa1799
Saiful I <saifulis.4965@gmail.com>
2025-01-13 20:04:22 +0530

    delete,edit

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     # path('contact/', contact),
     path('', include('core.urls')),
     path('items/', include('item.urls')),
+    path('dashboard/', include('dashboard.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ecommerce/item/forms.py
+++ ecommerce/item/forms.py
@@ -31,3 +31,28 @@ class NewItemForm(forms.ModelForm):
             }),
 
         }
+
+
+class EditItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ['name', 'price',
+                  'quantity', 'description', 'image', 'is_sold']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'price': forms.NumberInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'quantity': forms.NumberInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'description': forms.Textarea(attrs={
+                'class': INPUT_CLASSES
+            }),
+            'image': forms.FileInput(attrs={
+                'class': INPUT_CLASSES
+            }),
+
+        }
